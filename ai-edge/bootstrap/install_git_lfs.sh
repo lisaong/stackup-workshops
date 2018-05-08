baba638d316e7103faee5a849a91ffa0db75a589
Lisa Ong <lisaong@nus.edu.sg>
2018-05-08 06:16:32 +0000

    lfs bootstrap on raspberry pi

---
+++ ai-edge/bootstrap/install_git_lfs.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+install_git_lfs()
+{
+    set -e
+    # Get out if git-lfs is already installed
+    if $(git-lfs &> /dev/null); then
+        echo "git-lfs is already installed"
+        return
+    fi
+
+    GIT_LFS_BUILD=$HOME/.bin
+    # Install go 1.8 for ARMv6 (works also on ARMv7 & ARMv8)
+    sudo apt-get --yes --force-yes install git
+    rm -rf /usr/local/go
+    rm -rf $GIT_LFS_BUILD/go
+    mkdir -p $GIT_LFS_BUILD/go
+    pushd "$GIT_LFS_BUILD/go"
+        wget https://storage.googleapis.com/golang/go1.8.linux-armv6l.tar.gz -O go.tar.gz
+        sudo tar -C /usr/local -xzf go.tar.gz
+        rm go.tar.gz
+        export PATH=$PATH:/usr/local/go/bin
+        export GOPATH=$PWD
+        echo "PATH=$PATH:/usr/local/go/bin" >> $HOME/.bashrc
+        echo "GOPATH=$PWD" >> $HOME/.bashrc
+
+        # Download and compile git-lfs
+        go get github.com/github/git-lfs
+        sudo mv $GOPATH/bin/git-lfs /usr/bin/
+    popd
+    hash -r
+    git lfs install
+    set +e
+}
+install_git_lfs
