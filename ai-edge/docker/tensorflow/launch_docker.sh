62fd7d47ed8b6d044f36f7d1e313b8fe21c950aa
Lisa Ong <lisaong@nus.edu.sg>
2018-05-09 17:48:34 +0800

    Movidius 1 (#11)
    * fixed line endings
    
    * more dos to unix
    
    * initial Dockerfile
    
    * still WIP

--- ai-edge/docker/tensorflow/launch_docker.sh
+++ ai-edge/docker/tensorflow/launch_docker.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -it -p 18888:8888 -v ~/stackup-workshops/ai-edge:/notebooks/myNotebooks lisaong/rpi-docker-tensorflow:tf1.8.0 bash /run_jupyter.sh --allow-root
+docker run -it -p 18888:8888 -v ~/stackup-workshops/ai-edge:/notebooks/myNotebooks lisaong/rpi-docker-tensorflow:tf1.8.0
