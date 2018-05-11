2459a8f8730aaf39de2f621dc3cd2516f07a723b
Lisa Ong <lisaong@nus.edu.sg>
2018-05-11 15:43:03 +0000

    run ldconfig

--- ai-edge/docker/ell/pi/launch_docker.sh
+++ ai-edge/docker/ell/pi/launch_docker.sh
@@ -3,5 +3,4 @@ docker run -it \
   -v ~/stackup-workshops/ai-edge:/notebooks/myNotebooks \
   -v /opt/vc:/opt/vc \
   -p:28888:8888 \
-  lisaong/pi3-miniconda3-ell:2.3.3 \
-  bash
+  lisaong/pi3-miniconda3-ell:2.3.3
--- ai-edge/docker/ell/pi/run_jupyter.sh
+++ ai-edge/docker/ell/pi/run_jupyter.sh
@@ -13,5 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
-source activate root && /opt/conda/bin/jupyter notebook "$@"
+ldconfig
+source activate root
+/opt/conda/bin/jupyter notebook "$@"
