3b52a0cad1d8567708689ca37c9d9a81e977f11a
Lisa Ong <lisaong@nus.edu.sg>
2018-05-11 09:17:43 +0000

    WIP: jupyter on pi3 with conda

--- ai-edge/docker/ell/pi/jupyter_notebook_config.py
+++ ai-edge/docker/ell/pi/jupyter_notebook_config.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env bash
 # Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -13,5 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import os
+from IPython.lib import passwd
 
-source activate py3x && /opt/conda/bin/jupyter notebook "$@"
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.MultiKernelManager.default_kernel_name = 'python2'
+
+# sets a password if PASSWORD is set in the environment
+if 'PASSWORD' in os.environ:
+    c.NotebookApp.password = passwd(os.environ['PASSWORD'])
+    del os.environ['PASSWORD']
