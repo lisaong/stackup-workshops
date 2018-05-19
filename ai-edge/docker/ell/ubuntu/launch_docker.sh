f51554aade43c7f65093b29bf988b8fe3e4fd33c
Lisa Ong <lisaong@nus.edu.sg>
2018-05-19 10:51:35 +0800

    Actually added CNTK dependencies (#31)
    * Actually added CNTK dependencies
    
    * open mpi is too heavy weight

--- ai-edge/docker/ell/ubuntu/launch_docker.sh
+++ ai-edge/docker/ell/ubuntu/launch_docker.sh
@@ -3,3 +3,6 @@ docker run -it \
   -v `pwd`/scripts:/scripts \
   -v `pwd`/models:/tmp/models \
   lisaong/debian-miniconda3-ell:2.3.3 bash
+
+# To compile Open MPI (needed for CNTK):
+# root# cd /openmpi-1.10.3 && make -j all && make install
