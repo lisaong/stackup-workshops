19e545336921bded86d3c077608e4385d7932113
Lisa Ong <issohl@nus.edu.sg>
2018-10-05 14:00:16 +0800

    socket io doesn't seem to work

--- simple-ml/server/app.js
+++ simple-ml/server/app.js
@@ -1,24 +1,26 @@
 const http = require('http');
-const pyshell = require('python-shell')
+const {PythonShell} = require('python-shell')
 const hostname = '127.0.0.1';
 const port = 3000;
 
-// python process that loads the model and accepts stdin
-const model = new pyshell.PythonShell('call_model.py', { mode: 'text'});
-model.send('hello');
-
-// get stdout from the python process
-model.on('message', function(message){
-    console.log(message);
-})
-
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  model.send('hello');
-  res.end('Hello World\n');
+    spamOrHam('hello')
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
 });
 
+function spamOrHam(sms) {
+    options = {
+        args: ['--input', `"${sms}"`]
+    }
+
+    PythonShell.run('call_model.py', options, function (err, results) {
+        if (err) throw err;
+        console.log(results);
+    });
+}
+
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
--- simple-ml/server/call_model.py
+++ simple-ml/server/call_model.py
@@ -1,14 +1,25 @@
 import sys
+import argparse
 import pickle
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', help='Sms input (e.g. "hello :)").'
+    'If not provided, reads from STDIN')
+args = parser.parse_args()
 
 # load models
 vectorizer = pickle.load(open('../model/vectorizer.pickle', 'rb'))
 model = pickle.load(open('../model/model.pickle', 'rb'))
 
-# read input from stdin, return predictions to stdout
-line = sys.stdin.readline()
-while line:
-    Z_text = vectorizer.transform([line])
+def classify(input):
+    Z_text = vectorizer.transform([input])
     prediction = model.predict(Z_text.toarray())
     print(prediction[0])
+
+if args.input:
+    classify(args.input)
+else:
     line = sys.stdin.readline()
+    while line:
+        classify(line)
+        line = sys.stdin.readline()
