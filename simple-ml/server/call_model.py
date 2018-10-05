74c95082bb75318b805f0bd21dc1df85b75ea31a
Lisa Ong <issohl@nus.edu.sg>
2018-10-05 11:28:03 +0800

    predict using stdin

--- simple-ml/server/call_model.py
+++ simple-ml/server/call_model.py
@@ -1,9 +1,14 @@
+import sys
 import pickle
 
-# load vectorizer
+# load models
 vectorizer = pickle.load(open('../model/vectorizer.pickle', 'rb'))
-print(vectorizer)
+model = pickle.load(open('../model/model.pickle', 'rb'))
 
-# load model
-clf = pickle.load(open('../model/model.pickle', 'rb'))
-print(clf)
+# read input from stdin, return predictions to stdout
+line = sys.stdin.readline()
+while line:
+    Z_text = vectorizer.transform([line])
+    prediction = model.predict(Z_text.toarray())
+    print(prediction[0])
+    line = sys.stdin.readline()
