d4642165f3ba4a731046c0d03d76dfbaedfdda64
Lisa Ong <issohl@nus.edu.sg>
2018-10-05 14:35:38 +0800

    frameworkless form

--- simple-ml/server/app.js
+++ simple-ml/server/app.js
@@ -1,26 +1,61 @@
 const http = require('http');
-const {PythonShell} = require('python-shell')
+const { parse } = require('querystring');
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    spamOrHam('hello')
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
-});
-
-function spamOrHam(sms) {
+const {PythonShell} = require('python-shell')
+function spamOrHam(sms, callback) {
     options = {
         args: ['--input', `"${sms}"`]
     }
 
     PythonShell.run('call_model.py', options, function (err, results) {
         if (err) throw err;
-        console.log(results);
+        callback(results)
     });
 }
+
+// https://itnext.io/how-to-handle-the-post-request-body-in-node-js-without-using-a-framework-cd2038b93190
+const server = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
+            spamOrHam(result.sms, (prediction) => {
+                res.end(`${result.sms} is "${prediction}"`);
+            });
+        });
+    } 
+    else {
+        res.end(`
+            <!doctype html>
+            <html>
+            <body style="font-family:verdana">
+                <form action="/" method="post">
+                    <p>Enter an SMS message</p>
+                    <input type="text" name="sms" size="50"/><br />
+                    <button>Spam or Ham?</button>
+                </form>
+            </body>
+            </html>
+        `);
+    }
+});
 
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if (request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
