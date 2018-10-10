0f9c793bda330405d3467ce758ac6daae1cce5ec
Lisa Ong <issohl@nus.edu.sg>
2018-10-10 14:55:16 +0800

    cpu-only for eval (Raspberry Pi 3)

--- pi-pytorch/tutorials/sensor/eval.py
+++ pi-pytorch/tutorials/sensor/eval.py
@@ -1,14 +1,12 @@
 import torch
 from torch.autograd import Variable
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, classification_report
 import numpy as np
 import time
 
 # import our model and data
 from rnn import RNN
 from data import get_data
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 hidden_size = 10
 learning_rate = 0.01
@@ -18,7 +16,7 @@ sequence_length = 10
 batch_size = 32
 
 def load_model(input_size):
-    model = RNN(input_size, hidden_size, num_layers).to(device)
+    model = RNN(input_size, hidden_size, num_layers)
     checkpoint = torch.load('checkpoint.pt')
     model.load_state_dict(checkpoint['model_state_dict'])
     model.eval()
@@ -35,14 +33,16 @@ if __name__ == '__main__':
     input_size = X_train.shape[2] # batch, seq_len, input_size
 
     model = load_model(input_size)
-
-    inputs = Variable(X_test.float()).to(device)
+    inputs = Variable(X_test.float())
 
     tick = time.time()
     outputs = model(inputs)
     tock = time.time()
 
-    # probabilities => 0, 1
-    y_pred = (outputs.cpu().detach().numpy() > 0.5).astype(np.int)
-    print(confusion_matrix(y_test.values, y_pred))
+    # convert probabilities => 0 or 1
+    y_pred = (outputs.detach().numpy() > 0.5).astype(np.int)
+
     print('prediction time: %.3fs' % (tock - tick))
+
+    print(confusion_matrix(y_test.values, y_pred))
+    print(classification_report(y_test.values, y_pred))
