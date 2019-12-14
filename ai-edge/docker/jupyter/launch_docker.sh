1eca29df7e46c44de0e1abd7b54448ac8220752b
Lisa Ong <lisaong@nus.edu.sg>
2019-12-14 17:33:08 +0800

    run unattended

--- ai-edge/docker/jupyter/launch_docker.sh
+++ ai-edge/docker/jupyter/launch_docker.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-docker run -it \
+# Note: use -it for interactive
+#       use -d for daemon
+docker run -d \
   -v ~/stackup-workshops/ai-edge:/notebooks/myNotebooks \
   -p=48888:8888 \
   -e PASSWORD='a1-edge' \
