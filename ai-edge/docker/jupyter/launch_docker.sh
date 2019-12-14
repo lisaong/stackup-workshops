3a8397a5f995f749afdf7cc9eb9648a72805587a
Lisa Ong <lisaong@nus.edu.sg>
2019-12-14 17:43:24 +0800

    launch tensorboard

--- ai-edge/docker/jupyter/launch_docker.sh
+++ ai-edge/docker/jupyter/launch_docker.sh
@@ -3,8 +3,16 @@
 #       use -d for daemon
 mkdir -p ~/notebooks
 
+# Jupyter
 docker run -d \
   -v ~/notebooks:/notebooks \
   -p=48888:8888 \
   -e PASSWORD='a1-edge' \
   lisaong/rpi-jupyter-tf2-torch-py37:1.0
+
+# Tensorboard
+docker run -d \
+  -v ~/notebooks:/notebooks \
+  -p=46006:6006 \
+  lisaong/rpi-jupyter-tf2-torch-py37:1.0 \
+  tensorboard --logdir /notebooks/logs
