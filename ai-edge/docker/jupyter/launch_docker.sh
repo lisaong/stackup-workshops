231c0ecb1d622c15d96a2cb18d30f5aa7086d38e
Lisa Ong <lisaong@nus.edu.sg>
2019-12-14 17:38:15 +0800

    cleanup

--- ai-edge/docker/jupyter/launch_docker.sh
+++ ai-edge/docker/jupyter/launch_docker.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 # Note: use -it for interactive
 #       use -d for daemon
+mkdir -p ~/notebooks
+
 docker run -d \
-  -v ~/stackup-workshops/ai-edge:/notebooks/myNotebooks \
+  -v ~/notebooks:/notebooks \
   -p=48888:8888 \
   -e PASSWORD='a1-edge' \
   lisaong/rpi-jupyter-tf2-torch-py37:1.0
