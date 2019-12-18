c0fc6f75955406455b783b727c2676b6f57f62c7
Lisa Ong <lisaong@nus.edu.sg>
2019-12-18 17:03:12 +0800

    update container version

--- ai-edge/docker/jupyter/launch_docker.sh
+++ ai-edge/docker/jupyter/launch_docker.sh
@@ -8,11 +8,11 @@ docker run -d \
   -v ~/notebooks:/notebooks \
   -p=48888:8888 \
   -e PASSWORD='a1-edge' \
-  lisaong/rpi-jupyter-tf2-torch-py37:1.0
+  lisaong/rpi-jupyter-tf2-torch-py37:1.1
 
 # Tensorboard
 docker run -d \
   -v ~/notebooks:/notebooks \
   -p=46006:6006 \
-  lisaong/rpi-jupyter-tf2-torch-py37:1.0 \
+  lisaong/rpi-jupyter-tf2-torch-py37:1.1 \
   tensorboard --logdir /notebooks/logs --bind_all
