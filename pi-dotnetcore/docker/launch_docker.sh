4276a9a28408d369ce1fe05aa33bc9befd94c053
Lisa Ong <lisaong>
2020-01-01 21:06:09 +0800

    projects folder

--- pi-dotnetcore/docker/launch_docker.sh
+++ pi-dotnetcore/docker/launch_docker.sh
@@ -2,6 +2,8 @@
 # Note: use -it for interactive
 #       use -d for daemon
 
+mkdir -p ../projects
+
 docker run -it \
   -v ~/stackup-workshops/pi-dotnetcore/projects:/code \
   lisaong/rpi-dotnetcore-3:1.0
