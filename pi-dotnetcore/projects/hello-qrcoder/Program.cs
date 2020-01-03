1f963dda1c2db2b6c85f10eec12239e160f6b64a
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-01-04 00:37:21 +0800

    .NET core sample console app (#53)
    * added nuget package
    
    * added code
    
    * added libgdiplus
    
    * output to file
    
    * readme
    
    * print image to console

---
+++ pi-dotnetcore/projects/hello-qrcoder/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+// Documentation: https://github.com/codebude/QRCoder
+using QRCoder;
+
+namespace hello_qrcoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            QRCodeGenerator qrGenerator = new QRCodeGenerator();
+            QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", 
+                                        QRCodeGenerator.ECCLevel.Q);
+            using (var qrCode = new QRCode(qrCodeData))
+            {
+                using (var qrCodeImage = qrCode.GetGraphic(20))
+                {
+                    qrCodeImage.Save("output.png", ImageFormat.Png);
+                    ConsoleWriteImage(qrCodeImage);
+                }
+            }
+
+            Console.WriteLine("Generated QRCode!");
+        }
+
+        // https://stackoverflow.com/questions/33538527/display-a-image-in-a-console-application
+	public static void ConsoleWriteImage(Bitmap bmpSrc)
+	{
+	    int sMax = 39;
+	    decimal percent = Math.Min(decimal.Divide(sMax, bmpSrc.Width), decimal.Divide(sMax, bmpSrc.Height));
+	    Size resSize = new Size((int)(bmpSrc.Width * percent), (int)(bmpSrc.Height * percent));
+	    Func<System.Drawing.Color, int> ToConsoleColor = c =>
+	    {
+	        int index = (c.R > 128 | c.G > 128 | c.B > 128) ? 8 : 0; 
+	        index |= (c.R > 64) ? 4 : 0;
+	        index |= (c.G > 64) ? 2 : 0;
+	        index |= (c.B > 64) ? 1 : 0; 
+	        return index;
+	    };
+	    Bitmap bmpMin = new Bitmap(bmpSrc, resSize);
+	    for (int i = 0; i < resSize.Height; i++)
+	    {
+	        for (int j = 0; j < resSize.Width; j++)
+	        {
+	            Console.ForegroundColor = (ConsoleColor)ToConsoleColor(bmpMin.GetPixel(j, i));
+	            Console.Write("██");
+	        }
+	        System.Console.WriteLine();
+	    }
+	}
+    }
+}
---
+++ pi-dotnetcore/projects/hello-qrcoder/README.md
@@ -0,0 +1,30 @@
+### QRCoder Sample Console Application
+
+1. Clone QRCoder
+```
+cd projects
+git clone https://github.com/codebude/QRCoder
+```
+
+2. Launch Docker container
+```
+cd docker
+sh ./launch_docker.sh
+```
+
+3. Build QRCoder Nuget package
+```
+root@XXXX: cd /code/QRCoder/QRCoder
+root@XXXX: dotnet pack QRCoderProject.NETCore.sln
+```
+
+4. Add local Nuget source and install Nuget package
+```
+root@XXXX: cd /code/hello-qrcoder
+root@XXXX: dotnet add package QRCoder.NET --source /code/QRCoder/QRCoder/bin/Debug
+```
+
+5. Build and run
+```
+root@XXXX: dotnet run
+```
