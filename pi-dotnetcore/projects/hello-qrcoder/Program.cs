7e37882994a7a452f0a645293714e84d9f538c81
Ong Hwei-Min Lisa <issohl@nus.edu.sg>
2020-01-08 11:24:09 +0800

    ascii art ftw

--- pi-dotnetcore/projects/hello-qrcoder/Program.cs
+++ pi-dotnetcore/projects/hello-qrcoder/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Linq;
-using System.Drawing;
-using System.Drawing.Imaging;
 
 // Documentation: https://github.com/codebude/QRCoder
 using QRCoder;
@@ -17,42 +14,13 @@ namespace hello_qrcoder
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode("The text which should be encoded.", 
                                         QRCodeGenerator.ECCLevel.Q);
-            using (var qrCode = new QRCode(qrCodeData))
+            using (var qrCode = new AsciiQRCode(qrCodeData))
             {
-                using (var qrCodeImage = qrCode.GetGraphic(20))
-                {
-                    qrCodeImage.Save("output.png", ImageFormat.Png);
-                    ConsoleWriteImage(qrCodeImage);
-                }
+                string qrCodeAsAsciiArt = qrCode.GetGraphic(1);
+                Console.WriteLine(qrCodeAsAsciiArt);
             }
 
             Console.WriteLine("Generated QRCode!");
-        }
-
-        // https://stackoverflow.com/questions/33538527/display-a-image-in-a-console-application
-        public static void ConsoleWriteImage(Bitmap bmpSrc)
-        {
-            int sMax = 39;
-            decimal percent = Math.Min(decimal.Divide(sMax, bmpSrc.Width), decimal.Divide(sMax, bmpSrc.Height));
-            Size resSize = new Size((int)(bmpSrc.Width * percent), (int)(bmpSrc.Height * percent));
-            Func<System.Drawing.Color, int> ToConsoleColor = c =>
-            {
-                int index = (c.R > 128 | c.G > 128 | c.B > 128) ? 8 : 0; 
-                index |= (c.R > 64) ? 4 : 0;
-                index |= (c.G > 64) ? 2 : 0;
-                index |= (c.B > 64) ? 1 : 0; 
-                return index;
-            };
-            Bitmap bmpMin = new Bitmap(bmpSrc, resSize);
-            for (int i = 0; i < resSize.Height; i++)
-            {
-                for (int j = 0; j < resSize.Width; j++)
-                {
-                    Console.ForegroundColor = (ConsoleColor)ToConsoleColor(bmpMin.GetPixel(j, i));
-                    Console.Write("██");
-                }
-                System.Console.WriteLine();
-            }
         }
     }
 }
