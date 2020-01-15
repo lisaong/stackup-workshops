88e4a34980f9c3aafaecd88214d25ee9b8773c22
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-01-15 16:11:12 +0800

    Fetch and display headlines (#56)
    * WIP: fetch headlines
    
    * fixed serialization

--- pi-dotnetcore/projects/monit-hackernews/Data/NewsHeadline.cs
+++ pi-dotnetcore/projects/monit-hackernews/Data/NewsHeadline.cs
@@ -4,12 +4,28 @@ namespace monit_hackernews.Data
 {
     public class NewsHeadline
     {
-        public DateTime Date { get; set; }
+        public int id { get; set; }
 
-        public int Score { get; set; }
+        public int time { get; set; }
 
-        public string Title { get; set; }
+        public int score { get; set; }
 
-        public Uri Url { get; set; }
+        public string title { get; set; }
+
+        private string _url;
+        public string url 
+        {
+            get 
+            {
+                if (string.IsNullOrEmpty(_url))
+                {
+                    _url = String.Format("https://news.ycombinator.com/item?id={0}", id);
+                }
+                return _url;
+            }
+            set {
+                _url = value;
+            }
+        }
     }
 }
