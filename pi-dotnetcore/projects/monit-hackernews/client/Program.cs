cf772379dbb360c4280c4c8b5c2ba75d7a32203b
Lisa Ong <lisaong@nus.edu.sg>
2020-01-18 14:12:58 +0800

    WIP: disable SSL verification

--- pi-dotnetcore/projects/monit-hackernews/client/Program.cs
+++ pi-dotnetcore/projects/monit-hackernews/client/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 
@@ -6,13 +9,43 @@ namespace client
 {
     class Program
     {
+        bool ValidateSslCertificate(
+            HttpRequestMessage httpRequestMessage,
+            X509Certificate2 cert, X509Chain certChain,
+            SslPolicyErrors policyErrors)
+        {
+            // Validate the cert here and return true if it's correct.
+            // If this is a development app, you could just return true always
+            // In production you should ALWAYS either use a trusted cert or check the thumbprint of the cert matches one you expect.
+            return true;
+        }
+
         // https://dotnettutorials.net/lesson/async-main-csharp/
         static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
             var connection = new HubConnectionBuilder()
-                .WithUrl("https://localhost:5001/newshub")
+                .WithUrl("https://localhost:5001/newshub", options => {
+
+                    // Create a handler that accepts custom (untrusted) certificates
+                    var handler = new HttpClientHandler
+                    {
+                        ClientCertificateOptions = ClientCertificateOption.Manual,
+                        ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => 
+                        { 
+                            return ValidateSslCertificate(sender, cert, chain, sslPolicyErrors); 
+                        }
+                    };
+
+                    // Register the custom handler above and also configure WebSockets
+                    options.HttpMessageHandlerFactory = _ => handler;
+                    options.WebSocketConfiguration = sockets =>
+                    {
+                        sockets.RemoteCertificateValidationCallback = 
+                            new RemoteCertificateValidationCallback(ValidateSslCertificate);
+                    };
+                })
                 .WithAutomaticReconnect()
                 .Build();
 
