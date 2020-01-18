e03f904d83939fc528af7c8ec511618e0dc4174e
Lisa Ong <lisaong@nus.edu.sg>
2020-01-18 13:44:40 +0800

    compiles but crashes

--- pi-dotnetcore/projects/monit-hackernews/client/Program.cs
+++ pi-dotnetcore/projects/monit-hackernews/client/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace client
 {
     class Program
     {
-        static void Main(string[] args)
+        // https://dotnettutorials.net/lesson/async-main-csharp/
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:53353/newshub")
+                .WithAutomaticReconnect()
+                .Build();
+
+            connection.Closed += async (error) =>
+            {
+                await Task.Delay(new Random().Next(0,5) * 1000);
+                await connection.StartAsync();
+            };
+
+            await connection.StartAsync();
+            Console.WriteLine("Press any key to exist.");
+            Console.ReadKey();
         }
     }
 }
