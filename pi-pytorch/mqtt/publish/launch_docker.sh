1e8957b02b4e3c2e83a0559234cc71e205d6bbf4
Lisa Ong <lisaong@nus.edu.sg>
2020-02-03 08:05:42 +0000

    doc link

--- pi-pytorch/mqtt/publish/launch_docker.sh
+++ pi-pytorch/mqtt/publish/launch_docker.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://community.wia.io/d/15-accessing-the-host-from-inside-a-docker-container
 # --network="host" attaches the docker container to the local network
 # and allows querying of the ipaddresses of the host's network interfaces
 sudo docker run -it \
