3bdbccd1a79a17888d3e820993861e877a18cce7
Lisa Ong <lisaong@nus.edu.sg>
2020-02-06 08:58:55 +0000

    added udocker instructions

--- serverless/carmart/launch_docker.sh
+++ serverless/carmart/launch_docker.sh
@@ -3,3 +3,7 @@ docker run -it \
   --volume $(pwd):/code \
   lisaong/monitor-carmart:1.0 \
   bash -c "source bin/activate && python /code/monitor.py"
+
+# To run with udocker
+# source py2/bin/activate
+# udocker run lisaong/monitor-carmart:1.0
