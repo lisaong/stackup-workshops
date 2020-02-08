68cb329767feb94a85e810cf75af8f56796b6f92
Lisa Ong <lisaong@nus.edu.sg>
2020-02-08 05:07:19 +0000

    post to Slack

--- serverless/carmart/launch_docker.sh
+++ serverless/carmart/launch_docker.sh
@@ -1,6 +1,7 @@
 # map volume to allow override of python code
 docker run -it \
   --volume $(pwd):/code \
+  --env-file .env \
   lisaong/monitor-carmart:1.0 \
   bash -c "source bin/activate && python /code/monitor.py"
 
