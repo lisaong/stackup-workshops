13b5f56407e5feb20af5edc04882b40c7fd447f3
Lisa Ong <lisaong@nus.edu.sg>
2020-05-19 16:41:27 +0800

    reference env variable

--- learn-history/entrypoint.sh
+++ learn-history/entrypoint.sh
@@ -1,5 +1,5 @@
 # Execute the Jupyter notebook to train the model
-/opt/conda/bin/jupyter nbconvert --to notebook --execute $1
+/opt/conda/bin/jupyter nbconvert --to notebook --execute $NOTEBOOK_SRC
 
 # Run CI test
 python ci_test.py
