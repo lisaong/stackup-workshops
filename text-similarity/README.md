f833a4a03c0447a86702d65c0e16bb3a198cd226
Lisa Ong <lisaong@nus.edu.sg>
2020-05-20 09:48:14 +0800

    skeleton project

---
+++ text-similarity/README.md
@@ -0,0 +1,7 @@
+# Text Similarity
+
+![Docker Image CI](https://github.com/lisaong/stackup-workshops/workflows/Text%20Similarity%20CI/badge.svg)
+
+## Introduction
+
+*Note: This is a demo-oriented repo to teach you the basic concepts. We assume that you have a working knowledge with at least one programming language.*
---
+++ text-similarity/ci_test.py
@@ -0,0 +1,19 @@
+# tests for continuous integration of the model
+import unittest
+
+class ModelTestcase(unittest.TestCase):
+    def setUp(self):
+        """Called before every test case."""
+        pass
+
+    def tearDown(self):
+        """Called after every test case."""
+        pass
+
+    def testModel(self):
+        """Model test case."""
+        pass
+
+if __name__ == "__main__":
+    # run all tests
+    unittest.main()
