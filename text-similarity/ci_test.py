62a5188860e986ae02f9e002aabaedeb46438ce0
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-05-20 19:26:31 +0800

    Compute similarity with predefined keywords (#79)
    * scope workflows for pushes
    
    * add sklearn to visualise vectors
    
    * add initial spacy
    
    * talk abstract
    
    * similarity
    
    * add unit test
    
    * added test artifacts
    
    * clean text
    
    * init not called
    
    * try: resource allocation warning
    
    * generate similarity scores
    
    * checks
    
    * fix issues
    
    * added test artifacts
    
    * fix reference
    
    * Visualization using barplot
    
    * added plots
    
    * sample test
    
    * fixed colab reference

--- text-similarity/ci_test.py
+++ text-similarity/ci_test.py
@@ -1,10 +1,17 @@
 # tests for continuous integration of the model
 import unittest
+import pickle
 
 class ModelTestcase(unittest.TestCase):
     def setUp(self):
         """Called before every test case."""
-        pass
+        with open('ci_artifacts.pkl', 'rb') as f:
+            ci_artifacts = pickle.load(f)
+            self.text_original = ci_artifacts['text_original']
+            self.text_clean = ci_artifacts['text_clean']
+            self.text_vectors_clean = ci_artifacts['text_vectors_clean']
+            self.sample_scores = ci_artifacts['sample_scores']
+            self.raw_scores = ci_artifacts['raw_scores']
 
     def tearDown(self):
         """Called after every test case."""
@@ -12,7 +19,14 @@ class ModelTestcase(unittest.TestCase):
 
     def testModel(self):
         """Model test case."""
-        pass
+        assert len(self.text_original) > len(self.text_clean)
+        assert self.text_vectors_clean.shape[1] == 300
+        assert self.text_vectors_clean.shape[0] == 50 # truncated
+        assert len(self.sample_scores) == 3
+        assert len(self.raw_scores) == 1
+        print(self.sample_scores)
+        print(self.raw_scores)
+        print(f'Test Passed')
 
 if __name__ == "__main__":
     # run all tests
