ccc76ad0a4e46fd854f3c0976006d932a1fa30cf
Lisa Ong <lisaong@nus.edu.sg>
2020-05-20 18:23:22 +0800

    checks

--- text-similarity/ci_test.py
+++ text-similarity/ci_test.py
@@ -1,14 +1,18 @@
 # tests for continuous integration of the model
 import unittest
 import pickle
+import spacy
+from analysers import  KDramaSynopsisAnalyser
 
 class ModelTestcase(unittest.TestCase):
     def setUp(self):
         """Called before every test case."""
         with open('ci_artifacts.pkl', 'rb') as f:
-            self.ci_artifacts = pickle.load(f)
-            self.text_clean = self.ci_artifacts['text_clean']
-            self.text_vectors_clean = self.ci_artifacts['text_vectors_clean']
+            ci_artifacts = pickle.load(f)
+            self.text_original = ci_artifacts['text_original']
+            self.text_clean = ci_artifacts['text_clean']
+            self.text_vectors_clean = ci_artifacts['text_vectors_clean']
+            self.sample_scores = ci_artifacts['sample_scores']
 
     def tearDown(self):
         """Called after every test case."""
@@ -16,9 +20,12 @@ class ModelTestcase(unittest.TestCase):
 
     def testModel(self):
         """Model test case."""
-        print(self.text_clean)
+        assert len(self.text_original) > len(self.text_clean)
         assert self.text_vectors_clean.shape[1] == 300
-        print(f'Test Passed: loaded vectors of shape: {self.text_vectors_clean.shape}')
+        assert self.text_vectors_clean.shape[0] == 50 # truncated
+        assert len(self.sample_scores) == 3
+        print(self.sample_scores)
+        print(f'Test Passed')
 
 if __name__ == "__main__":
     # run all tests
