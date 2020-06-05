2cf5a0b2a431207edf1e904530c2acb285538a0b
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-06-05 12:02:50 +0800

    Learn history talk updates (#83)
    * switched to talk dataset
    
    * update ci test
    
    * WIP: test failure is  hidden
    
    * make test pass
    
    * uncover hidden test failures
    
    * tf 2 is default on Colab now
    
    * outline
    
    * complete demo
    
    * load model test
    
    * use df.drop
    
    * flush

--- learn-history/ci_test.py
+++ learn-history/ci_test.py
@@ -1,16 +1,17 @@
 # tests for continuous integration of the model
 import unittest
-import pandas as pd
-
-# the test assumes Colab notebook downloaded this CSV file
-# so we don't have to download again.
-DATASET_CSV = 'weekly-infectious-disease-bulletin-cases.csv'
+import pickle
+import tensorflow as tf
+from sklearn.metrics import mean_squared_error,  mean_absolute_error
 
 # https://docs.python.org/3.7/library/unittest.html
 class ModelTestcase(unittest.TestCase):
     def setUp(self):
         """Called before every test case."""
-        self.df = pd.read_csv(DATASET_CSV)
+        with open('ci_artifacts.pkl', 'rb') as f:
+            ci_artifacts = pickle.load(f)
+            self.df_windowed = ci_artifacts['df_windowed']
+            self.model_filename = ci_artifacts['model_filename']
 
     def tearDown(self):
         """Called after every test case."""
@@ -18,8 +19,15 @@ class ModelTestcase(unittest.TestCase):
 
     def testModel(self):
         """Model test case."""
-        assert self.df.shape[0] > 0
-        print(f'Test Passed: dataset has {self.df.shape[0]} rows')
+        y = self.df_windowed['t-3']
+        X = self.df_windowed.drop(columns=['t-3'])
+
+        model = tf.keras.models.load_model(self.model_filename)
+        y_pred = model.predict(X)
+        mse = mean_squared_error(y, y_pred)
+        mae = mean_absolute_error(y, y_pred)
+
+        print(f'Test Passed: MSE: {mse}, MAE: {mae}')
 
 if __name__ == "__main__":
     # run all tests
--- learn-history/entrypoint.sh
+++ learn-history/entrypoint.sh
@@ -3,9 +3,12 @@
 # Execute the Jupyter notebook to train the model
 /opt/conda/bin/jupyter nbconvert --to notebook --execute $NOTEBOOK_SRC
 
-# Run CI test
-python ci_test.py
-
 # Copy artifacts and list them
 cp *.pkl /artifacts/.
+cp *.h5 /artifacts/.
 ls -alR /artifacts
+
+# Run CI test
+# (This should be the last line in the script so that test
+# failures are not hidden)
+python ci_test.py
