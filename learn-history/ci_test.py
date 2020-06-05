f6f128e04d0cfa14407e355a36d7100df110fc76
Lisa Ong <lisaong@nus.edu.sg>
2020-06-05 20:53:32 +0800

    add classifier test

--- learn-history/ci_test.py
+++ learn-history/ci_test.py
@@ -2,7 +2,8 @@
 import unittest
 import pickle
 import tensorflow as tf
-from sklearn.metrics import mean_squared_error,  mean_absolute_error
+from sklearn.metrics import mean_squared_error, \
+    mean_absolute_error, classification_report
 
 # https://docs.python.org/3.7/library/unittest.html
 class ModelTestcase(unittest.TestCase):
@@ -12,6 +13,9 @@ class ModelTestcase(unittest.TestCase):
             ci_artifacts = pickle.load(f)
             self.df_windowed = ci_artifacts['df_windowed']
             self.model_filename = ci_artifacts['model_filename']
+
+            self.df_pattern = ci_artifacts['df_pattern']
+            self.model_pattern_filename = ci_artifacts['model_pattern']
 
     def tearDown(self):
         """Called after every test case."""
@@ -28,6 +32,17 @@ class ModelTestcase(unittest.TestCase):
         mae = mean_absolute_error(y, y_pred)
 
         print(f'Test Passed: MSE: {mse}, MAE: {mae}')
+
+    def testPatternRecognitionModel(self):
+        """Model test case."""
+        y = self.df_pattern['recession']
+        X = self.df_windowed.drop(columns=['recession'])
+
+        model = tf.keras.models.load_model(self.model_pattern_filename)
+        y_pred = model.predict(X) >= 0.5
+        rpt = classification_report(y, y_pred)
+
+        print(f'Test Passed:\n{rpt}')
 
 if __name__ == "__main__":
     # run all tests
