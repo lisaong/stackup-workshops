5c168d784caa96c70224d98f6d68b8e793beb119
Lisa Ong <lisaong@nus.edu.sg>
2020-06-05 08:02:45 +0800

    WIP: test failure is  hidden

--- learn-history/entrypoint.sh
+++ learn-history/entrypoint.sh
@@ -3,9 +3,9 @@
 # Execute the Jupyter notebook to train the model
 /opt/conda/bin/jupyter nbconvert --to notebook --execute $NOTEBOOK_SRC
 
-# Run CI test
-python ci_test.py
-
 # Copy artifacts and list them
 cp *.pkl /artifacts/.
 ls -alR /artifacts
+
+# Run CI test
+python ci_test.py
