11656de191f115002b8d9500a6a6473a1e2a235f
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-07-10 21:32:18 +0800

    Mask no mask classifier (#87)
    * Created using Colaboratory
    
    * added data
    
    * old file
    
    * Created using Colaboratory
    
    * updated gitignore
    
    * Created using Colaboratory
    
    * Created using Colaboratory
    
    * renamed
    
    * add CI
    
    * add data
    
    * add unzip
    
    * test failure and numpy compatibility warning
    
    * updated numpy version
    
    * undo
    
    * reference master
    
    * copy models
    
    * Readme

---
+++ mask-or-not/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+IFS=';'
+read -ra notebooks <<< "$NOTEBOOK_SRC"
+
+# Execute the Jupyter notebook to train the model
+for notebook in "${notebooks[@]}"; do
+    echo $notebook
+    /opt/conda/bin/jupyter nbconvert --to notebook --execute $notebook
+done
+
+# Copy artifacts and list them
+cp *.pkl /artifacts/.
+cp *.h5 /artifacts/.
+ls -alR /artifacts
+
+# Run CI test
+# (This should be the last line in the script so that test
+# failures are not hidden)
+python ci_test.py
