6155fb179a63dc0efe5cec7b295a4600dfa37e63
Lisa Ong <lisaong@nus.edu.sg>
2020-07-15 23:38:00 +0800

    shrink to 20x20

--- mask-or-not/run_model.ino
+++ mask-or-not/run_model.ino
@@ -1,12 +1,13 @@
 #include "EloquentTinyML.h"
 #include "mlp_full.h"
 
-#define NUMBER_OF_INPUTS 900
+#define NUMBER_OF_INPUTS 400
 #define NUMBER_OF_OUTPUTS 1
 
 // in future projects you may need to tweak this value
 // it's a trial and error process
-#define TENSOR_ARENA_SIZE 5*1024
+// #define TENSOR_ARENA_SIZE 5*1024
+#define TENSOR_ARENA_SIZE 5*512
 
 Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml;
 
