578eda101de95660e45b2242671e8a01c29b3a9d
Lisa Ong <lisaong@nus.edu.sg>
2020-07-15 22:20:33 +0800

    arduino code

---
+++ mask-or-not/run_model.ino
@@ -0,0 +1,30 @@
+// https://eloquentarduino.github.io/2020/01/easy-tinyml-on-esp32-and-arduino/
+#include "EloquentTinyML.h"
+#include "mlp_full.h"
+// #include "sine_model.h"
+
+#define NUMBER_OF_INPUTS 900
+// #define NUMBER_OF_INPUTS 1
+#define NUMBER_OF_OUTPUTS 1
+
+// in future projects you may need to tweak this value
+// it's a trial and error process
+// #define TENSOR_ARENA_SIZE 58*1024
+#define TENSOR_ARENA_SIZE 2*1024
+
+// https://github.com/eloquentarduino/EloquentTinyML/issues/3
+Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml((unsigned char*)sine_model);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  float input[NUMBER_OF_INPUTS];
+
+  Serial.println("predicting...");
+  float predicted = ml.predict(input);
+  Serial.print("predicted ");
+  Serial.println(predicted);
+  delay(1000);
+}
