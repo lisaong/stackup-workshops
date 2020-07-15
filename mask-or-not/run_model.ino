4cd109464115902d2bcec607776b2e80d07f5186
Lisa Ong <lisaong@nus.edu.sg>
2020-07-15 22:34:48 +0800

    cleanup

--- mask-or-not/run_model.ino
+++ mask-or-not/run_model.ino
@@ -1,17 +1,11 @@
-// https://eloquentarduino.github.io/2020/01/easy-tinyml-on-esp32-and-arduino/
 #include "EloquentTinyML.h"
-
-// Copied to Documents/Arduino/libraries/mask_or_not
 #include "mlp_full.h"
-// #include "sine_model.h"
 
 #define NUMBER_OF_INPUTS 900
-// #define NUMBER_OF_INPUTS 1
 #define NUMBER_OF_OUTPUTS 1
 
 // in future projects you may need to tweak this value
 // it's a trial and error process
-// #define TENSOR_ARENA_SIZE 58*1024
 #define TENSOR_ARENA_SIZE 2*1024
 
 // https://github.com/eloquentarduino/EloquentTinyML/issues/3
