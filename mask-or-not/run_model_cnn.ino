b48585464bc372eda8d6fb0db54f025c0f07ebf8
Lisa Ong <lisaong@nus.edu.sg>
2020-07-21 16:24:44 +0800

    add memory footprint logging

--- mask-or-not/run_model_cnn.ino
+++ mask-or-not/run_model_cnn.ino
@@ -5,12 +5,22 @@
 
 // in future projects you may need to tweak this value
 // it's a trial and error process
-#define TENSOR_ARENA_SIZE 2*1024
+#define TENSOR_ARENA_SIZE 5*1024
 
 Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml;
 
+void logMemory() {
+  // https://thingpulse.com/esp32-how-to-use-psram/
+  // To see the log in Serial Monitor: Tools > Core Debug Level > Verbose
+  log_d("Total heap: %d", ESP.getHeapSize());
+  log_d("Free heap: %d", ESP.getFreeHeap());
+  // log_d("Total PSRAM: %d", ESP.getPsramSize());
+  // log_d("Free PSRAM: %d", ESP.getFreePsram());  
+}
+
 void setup() {
   Serial.begin(115200);
+  logMemory();
   ml.begin((unsigned char*)model_data);
 }
 
@@ -28,5 +38,6 @@ void loop() {
   float predicted = ml.predict(input);
   Serial.print("predicted: ");
   Serial.println(predicted);
+  logMemory();
   delay(1000);
 }
