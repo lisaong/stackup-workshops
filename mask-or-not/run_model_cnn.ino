9a546d4f4640dd1eb1e4c90b4466f4dcb9c2d4a1
Lisa Ong <11318241+lisaong@users.noreply.github.com>
2020-07-27 18:07:37 +0800

    Send image over Bluetooth Serial (#93)
    * WIP: bluetooth serial comm
    
    * updated for MacOS
    
    * update py script
    
    * updated ignore
    
    * send image
    
    * dump image data
    
    * fix compile issue
    
    * updated note
    
    * updated sketch and README
    
    * cleanup
    
    * cleanup script
    
    * fix merge conflict
    
    Co-authored-by: Lisa Ong <>

--- mask-or-not/run_model_cnn.ino
+++ mask-or-not/run_model_cnn.ino
@@ -1,3 +1,4 @@
+#include <BluetoothSerial.h>
 #include "EloquentTinyML.h"
 
 // Schema: model_name_{input_size}.h
@@ -7,37 +8,72 @@
 // it's a trial and error process
 #define TENSOR_ARENA_SIZE 5*1024
 
-Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> ml;
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+// Globals
+BluetoothSerial Bluetooth;
+Eloquent::TinyML::TfLite<NUMBER_OF_INPUTS, NUMBER_OF_OUTPUTS, TENSOR_ARENA_SIZE> Model;
+float ImageData[NUMBER_OF_INPUTS];
 
 void logMemory() {
   // https://thingpulse.com/esp32-how-to-use-psram/
   // To see the log in Serial Monitor: Tools > Core Debug Level > Verbose
   log_d("Total heap: %d", ESP.getHeapSize());
   log_d("Free heap: %d", ESP.getFreeHeap());
-  // log_d("Total PSRAM: %d", ESP.getPsramSize());
-  // log_d("Free PSRAM: %d", ESP.getFreePsram());  
+}
+
+void callback(esp_spp_cb_event_t event, esp_spp_cb_param_t *param){
+  if (event == ESP_SPP_SRV_OPEN_EVT){
+    Serial.println("Client Connected");
+  }
+
+  if (event == ESP_SPP_CLOSE_EVT ){
+    Serial.println("Client disconnected");
+  }
+}
+
+void onDataCallback(const uint8_t *buffer, size_t size) {
+  Serial.printf("onDataCallback(%p, %d)", buffer, size);
+  if (size == NUMBER_OF_INPUTS) {
+    for (size_t i=0; i<size; i++) {
+      Serial.printf("%d,", buffer[i]);
+
+      // perform scaling
+      ImageData[i] = (buffer[i]-127)/127.0;
+    }
+
+    Serial.println("\npredicting...");
+    float prediction = Model.predict(ImageData);
+    Serial.printf("prediction: %.2f\n", prediction);
+  }
 }
 
 void setup() {
-  Serial.begin(115200);
   logMemory();
-  ml.begin((unsigned char*)model_data);
+
+  Serial.begin(115200);
+
+  Bluetooth.register_callback(callback);
+
+  // For this to compile, you need the latest version of esp32-arduino
+  // refer to README.md
+  Bluetooth.onData(onDataCallback);
+
+  if (!Bluetooth.begin("ESP32 Bluetooth")) {
+    Serial.println("Could not start bluetooth");
+  } else {
+    Serial.println("Bluetooth started");
+  }
+
+  Model.begin((unsigned char*)model_data);
+
+  logMemory();
 }
 
 void loop() {
-  float input[NUMBER_OF_INPUTS];
-
-  for (int i=0; i<NUMBER_OF_INPUTS; i++) {
-    input[i] = random(-127, 127)/127.0;
-    Serial.print(input[i]);
-    Serial.print(",");
-  }
-  Serial.println("");
-
-  Serial.println("predicting...");
-  float predicted = ml.predict(input);
-  Serial.print("predicted: ");
-  Serial.println(predicted);
-  logMemory();
-  delay(1000);
+  // Everything is done in the data callback
+  // because the default Bluetooth Serial Port RX buffer is
+  // hardcoded to 512 bytes
 }
